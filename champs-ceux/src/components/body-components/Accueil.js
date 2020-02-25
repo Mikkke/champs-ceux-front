5d2f483cdaff168ab1e15bd399076f67b6475aed
Mikkke <jkellymichael@gmail.com>
2020-02-25 22:00:58 +0100

    add background picture on accueil

--- champs-ceux/src/components/body-components/Accueil.js
+++ champs-ceux/src/components/body-components/Accueil.js
@@ -2,8 +2,11 @@ import React from "react";
 
 const Accueil = () => {
   return (
-    <div>
-      <h1>Acceuil pour voir</h1>
+    <div className="accueil">
+      <div>
+        <h2>Des bons produits fermier a petit prix</h2>
+        <p>Voir nos produits</p>
+      </div>
     </div>
   );
 };
--- champs-ceux/src/components/nav/Nav.js
+++ champs-ceux/src/components/nav/Nav.js
@@ -5,11 +5,11 @@ import { Link } from "react-router-dom";
 const Nav = () => {
   return (
     <nav className="nav-header">
-      <Link to="/">Accueil</Link>
-      <Link to="/produits">Produits</Link>
-      <Link to="/contact">Contact</Link>
-      <Link to="/compte">Compte</Link>
-      <Link to="/panier">Panier</Link>
+      <Link to="/">ACCEUIL</Link>
+      <Link to="/produits">PRODUITS</Link>
+      <Link to="/contact">CONTACT</Link>
+      <Link to="/compte">COMPTE</Link>
+      <Link to="/panier">PANIER</Link>
     </nav>
   );
 };
