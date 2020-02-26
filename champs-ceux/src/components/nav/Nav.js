dc8b743ab1d01bca5e1493728941fe9232b6f95d
Mikkke <jkellymichael@gmail.com>
2020-02-26 09:40:24 +0100

    update nav file

--- champs-ceux/src/components/nav/Nav.js
+++ champs-ceux/src/components/nav/Nav.js
@@ -5,7 +5,7 @@ import { Link } from "react-router-dom";
 const Nav = () => {
   return (
     <nav className="nav-header">
-      <Link to="/">ACCEUIL</Link>
+      <Link to="/">ACCUEIL</Link>
       <Link to="/produits">PRODUITS</Link>
       <Link to="/contact">CONTACT</Link>
       <Link to="/compte">COMPTE</Link>
