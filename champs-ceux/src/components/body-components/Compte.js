769da34b4414fb578dc9912aeb45677445f29a61
Mikkke <jkellymichael@gmail.com>
2020-02-27 17:22:12 +0100

    compte.js
    mise en place de la route post pour ajouter un produit

--- champs-ceux/src/components/body-components/Compte.js
+++ champs-ceux/src/components/body-components/Compte.js
@@ -1,9 +1,48 @@
 import React from "react";
+import { useForm } from "react-hook-form";
+import * as yup from "yup";
+import axios from "axios";
+
+const schema = yup.object().shape({
+  nom: yup
+    .string()
+    .max(30, "le nom doit comporter 30 caracteres max")
+    .required("ce champs est requis")
+});
 
 const Compte = () => {
+  const { register, handleSubmit, errors } = useForm({
+    validationSchema: schema
+  });
+
+  const onSubmit = async data => {
+    console.log(data);
+    try {
+      const res = await axios.post("http://localhost:8080/api/produits", data);
+      console.log(res.data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
     <div>
-      <h1>Sur le compte</h1>
+      <h1>Mon Compte</h1>
+      <div>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <label>Nom : </label> <input ref={register} name="nom" type="text" />
+          {errors.nom && errors.nom.message}
+          <label>Prix : </label>{" "}
+          <input ref={register} name="prix" type="number" />
+          <label>Quantité : </label>{" "}
+          <input ref={register} name="quantite" type="number" />
+          <label>Photo : </label>{" "}
+          <input ref={register} name="photo" type="text" />
+          <label>Description : </label>{" "}
+          <textarea ref={register} name="description" />
+          <input type="submit" value="ajouter a la liste" />
+        </form>
+      </div>
     </div>
   );
 };
--- champs-ceux/src/components/body-components/Produits.js
+++ champs-ceux/src/components/body-components/Produits.js
@@ -1,9 +1,64 @@
-import React from "react";
+import React, { useState } from "react";
+import {
+  Card,
+  CardImg,
+  CardText,
+  CardBody,
+  CardTitle,
+  CardSubtitle,
+  Button
+} from "reactstrap";
+
+const apiBaseURL = process.env.REACT_APP_BASE_API;
+const initialUrl = `${apiBaseURL}/api/produits`;
 
 const Produits = () => {
+  const [produit, setProduit] = useState([]);
+  console.log("composant produit", produit);
+
+  const getProduitData = url => {
+    fetch(url)
+      .then(res => res.json())
+      .then(data => {
+        //const [{ nom, prix, quantite, description, photo }] = data;
+
+        setProduit(data);
+        console.log(data, "data");
+      })
+      .catch(err => console.error("vous avez une erreur", err));
+  };
+
+  React.useEffect(() => {
+    getProduitData(initialUrl);
+  }, []);
+
   return (
-    <div>
-      <h1> Sur les Produits</h1>
+    <div className="produit-div">
+      {produit.length === 0 ? (
+        <h1>Chargement...</h1>
+      ) : (
+        produit.map((el, index) => {
+          console.log(`${apiBaseURL}${el.photo}`);
+          return (
+            <div key={index} className="my-card">
+              <Card>
+                <CardImg
+                  top
+                  width="100%"
+                  src={`${apiBaseURL}${el.photo}`}
+                  alt="Card image cap"
+                />
+                <CardBody>
+                  <CardTitle>{el.nom}</CardTitle>
+                  <CardSubtitle>{el.prix}</CardSubtitle>
+                  <CardText>{el.quantite}</CardText>
+                  <Button variant="contained" color="primary"></Button>
+                </CardBody>
+              </Card>
+            </div>
+          ); //<p key={index}>{el.nom}</p>;
+        })
+      )}
     </div>
   );
 };
